945b38af8322bad14f9d190ae5078ccb17a98426
cake <3rdbinarybug@gmail.com>
2024-03-24 06:49:05 -0400

    Updated collatz_graph.py
    added basic algo script for debugging
    
    working on iteration frequency analysis for iteration frequencies returned by collatz(), though my algo isnt returning accurate data. i'll need to rewrite it to plot x axis values as the frequency label rather than as x to get the data i'm looking for. maybe im overthinking it...

--- collatz_graph.py
+++ collatz_graph.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import time
+from time import time
 from sys import setrecursionlimit
 setrecursionlimit(10_000)
 
@@ -19,10 +19,12 @@ class cg():
         step += 1
 
         return self.collatz(x, step)
+    
+    print(collatz(121))
 
     def run_collatz(self, x, rng, cache): # run the conjecture in a loop
-        # paths = 0 # TODO: unique pathfinder
         y_points = []
+        # paths = 0 # TODO: unique pathfinder
 
         for x in range(x, rng):
             step = self.collatz(x)
@@ -33,9 +35,9 @@ class cg():
 
     def plot_points(self, start_val, stop_val, display=True, save=False):
         print(f'Running calculations for {start_val:,} through {stop_val:,}...')
-        start_time = time.time()
+        start_time = time()
         y_points = self.run_collatz(start_val, stop_val, self.global_cache)
-        time_taken = time.time() - start_time
+        time_taken = time() - start_time
         x_points = [x for x in range(start_val, stop_val)]
         print(f'Took {time_taken:.2f} seconds...')
 
@@ -56,4 +58,55 @@ class cg():
             print('Displaying plot...')
             plt.show() # show graph in gui
 
-cg().plot_points(1, 10_000_000, display=True, save=True) # for stable execution do not exceed a 1,000,000 unit gap
+    def step_freq(self, start_val, stop_val):
+        print(f'Running Calculations for {start_val:,} through {stop_val:,}...')
+        steps = self.run_collatz(start_val, stop_val, self.global_cache)
+        data = {}
+
+        for step in steps:
+            if step in data:
+                data[step] += 1
+            else:
+                data[step] = 1
+
+        return data
+
+    def plot_step_freq(self, start_val, stop_val, display=True, save=False):
+        print(f'Running Calculations for {start_val:,} through {stop_val:,}...')
+        start_time = time()
+        steps = self.run_collatz(start_val, stop_val, self.global_cache)
+        time_taken = time() - start_time
+        print(f'Took {time_taken:.2f} seconds...')
+
+        print('Parsing data...')
+        data = {}
+        for step in steps:
+            if step in data:
+                data[step] += 1
+            else:
+                data[step] = 1
+
+        print('Dumping cache and sorting data...')
+        data = sorted(list(data.items()))
+        x_points = [float(t[0]) for t in data]
+        y_points = [float(t[1]) for t in data]
+
+        # print(x_points)
+        # print(y_points)
+
+        print('Plotting coordinates...')
+        plt.plot(x_points, y_points, marker=',', linestyle='')
+        plt.xlabel('Loop Length') # set X axis label on graph
+        plt.ylabel('Frequency') # set Y axis label on graph
+        plt.title(f'Collatz Conjecture | Iteration Frequency Analysis | Calculations took {time_taken:.1f} secs')
+
+        if save:
+            print('Saving plot to image...')
+            plt.savefig(f'Collatz_Array_IFA_{start_val}_{stop_val}_{time_taken:.0f}secs.png') # save graph to png
+
+        if display:
+            print('Displaying plot...')
+            plt.show() # show graph in gui
+
+cg().plot_step_freq(1, 10_000_000, display=True, save=False)
+# cg().plot_points(1, 1_000_000, display=True, save=True) # for stable execution do not exceed a 1,000,000 unit gap
