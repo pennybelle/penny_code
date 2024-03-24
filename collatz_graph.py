242e18de66d12bc6256cf4df0356a64105eed775
cake <3rdbinarybug@gmail.com>
2024-03-24 07:49:04 -0400

    Update collatz_graph.py
    code cleaning/optimization

--- collatz_graph.py
+++ collatz_graph.py
@@ -19,8 +19,6 @@ class cg():
         step += 1
 
         return self.collatz(x, step)
-    
-    print(collatz(121))
 
     def run_collatz(self, x, rng, cache): # run the conjecture in a loop
         y_points = []
@@ -33,50 +31,40 @@ class cg():
 
         return y_points
 
-    def plot_points(self, start_val, stop_val, display=True, save=False):
-        print(f'Running calculations for {start_val:,} through {stop_val:,}...')
+    def plot_collatz(self, start, end, display=True, save=False):
+        print(f'Running calculations for {start:,} through {end:,}...')
         start_time = time()
-        y_points = self.run_collatz(start_val, stop_val, self.global_cache)
-        time_taken = time() - start_time
-        x_points = [x for x in range(start_val, stop_val)]
-        print(f'Took {time_taken:.2f} seconds...')
+        y_points = self.run_collatz(start, end, self.global_cache)
+        secs = time() - start_time # keep track of calculation time
+        x_points = [x for x in range(start, end)]
+        print(f'Took {secs:.2f} seconds...')
 
         print('Dumping cache...')
-        self.global_cache.clear()
+        self.global_cache.clear() # sweet relief
 
         print('Plotting coordinates...')
         plt.plot(x_points, y_points, marker=',', linestyle='')
         plt.xlabel('Loop Start Value') # set X axis label on graph
         plt.ylabel('Loop Range') # set Y axis label on graph
-        plt.title(f'Collatz Conjecture | Plotting {start_val:,} to {stop_val:,} | Calculations took {time_taken:.1f} secs')
+        plt.title(f'Collatz Conjecture | Plotting {start:,} to {end:,} | Calculations took {secs:.1f} secs')
 
         if save:
             print('Saving plot to image...')
-            plt.savefig(f'Collatz_Array_{start_val}_{stop_val}_{time_taken:.0f}secs.png') # save graph to png
+            plt.savefig(f'Collatz_Array_{start}_{end}_{secs:.0f}secs.png') # save graph to png
 
         if display:
             print('Displaying plot...')
             plt.show() # show graph in gui
 
-    def step_freq(self, start_val, stop_val):
-        print(f'Running Calculations for {start_val:,} through {stop_val:,}...')
-        steps = self.run_collatz(start_val, stop_val, self.global_cache)
-        data = {}
-
-        for step in steps:
-            if step in data:
-                data[step] += 1
-            else:
-                data[step] = 1
-
-        return data
-
-    def plot_step_freq(self, start_val, stop_val, display=True, save=False):
-        print(f'Running Calculations for {start_val:,} through {stop_val:,}...')
+    def plot_step_freq(self, start, end, display=True, save=False):
+        print(f'Running Calculations for {start:,} through {end:,}...')
         start_time = time()
-        steps = self.run_collatz(start_val, stop_val, self.global_cache)
-        time_taken = time() - start_time
-        print(f'Took {time_taken:.2f} seconds...')
+        steps = self.run_collatz(start, end, self.global_cache)
+        secs = time() - start_time # keep track of calculation time
+        print(f'Took {secs:.2f} seconds...')
+
+        print('Dumping cache...')
+        self.global_cache.clear() # sweet relief
 
         print('Parsing data...')
         data = {}
@@ -86,27 +74,25 @@ class cg():
             else:
                 data[step] = 1
 
-        print('Dumping cache and sorting data...')
-        data = sorted(list(data.items()))
-        x_points = [float(t[0]) for t in data]
-        y_points = [float(t[1]) for t in data]
-
-        # print(x_points)
-        # print(y_points)
+        print('Sorting data and dumping local cache...')
+        data = sorted(list(data.items())) # convert dict(data) to sorted list of dict(data) contents
+        x_points = [float(t[0]) for t in data] # parse nested tuples for value in index 0
+        y_points = [float(t[1]) for t in data] # parse nested tuples for value in index 1
+        data = [] # dump old data for memory optimization
 
         print('Plotting coordinates...')
-        plt.plot(x_points, y_points, marker=',', linestyle='')
-        plt.xlabel('Loop Length') # set X axis label on graph
+        plt.plot(x_points, y_points, marker='.', markersize=1, linestyle='')
+        plt.xlabel('Sequence Length') # set X axis label on graph
         plt.ylabel('Frequency') # set Y axis label on graph
-        plt.title(f'Collatz Conjecture | Iteration Frequency Analysis | Calculations took {time_taken:.1f} secs')
+        plt.title(f'Collatz Conjecture | Sequence Length Analysis [{start}, {end}] | Calculations took {secs:.1f} secs')
 
         if save:
             print('Saving plot to image...')
-            plt.savefig(f'Collatz_Array_IFA_{start_val}_{stop_val}_{time_taken:.0f}secs.png') # save graph to png
+            plt.savefig(f'Collatz_Array_SFA_{start}_{end}_{secs:.0f}secs.png') # save graph to png
 
         if display:
             print('Displaying plot...')
             plt.show() # show graph in gui
 
 cg().plot_step_freq(1, 10_000_000, display=True, save=False)
-# cg().plot_points(1, 1_000_000, display=True, save=True) # for stable execution do not exceed a 1,000,000 unit gap
+# cg().plot_collatz(1, 1_000_000, display=True, save=True) # for stable execution do not exceed a 1,000,000 unit gap
